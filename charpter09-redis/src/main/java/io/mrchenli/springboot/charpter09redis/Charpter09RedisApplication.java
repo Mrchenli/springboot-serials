7e264d32c43861f63d35cba2ac835832008f53e8
Mrchenli <1278530889@qq.com>
2017-08-24 22:53:11 +0800

    charpter09-redis

---
+++ charpter09-redis/src/main/java/io/mrchenli/springboot/charpter09redis/Charpter09RedisApplication.java
@@ -0,0 +1,12 @@
+package io.mrchenli.springboot.charpter09redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Charpter09RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Charpter09RedisApplication.class, args);
+	}
+}
